5957edd790de1a9ec08a248779ef80f17b0d1873
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-19 19:46:35 +0530

    Update add_user.py

--- VideoEncoder/utils/database/add_user.py
+++ VideoEncoder/utils/database/add_user.py
@@ -22,8 +22,8 @@ from .access_db import db
 
 
 async def AddUserToDatabase(bot: Client, cmd: Message):
-    if not await db.is_user_exist(cmd.from_user.id):
-        await db.add_user(cmd.from_user.id)
+    if not await db.is_user_exist(cmd.from_chat.id):
+        await db.add_user(cmd.from_chat.id)
         if log is not None:
             await bot.send_message(
                 int(log),
